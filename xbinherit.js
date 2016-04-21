fc110e28e4cfe025504f3ae59aebd3e18efc8f0e
riderking <riderkingkii@msn.com>
2016-04-21 21:18:24 +0800

    Create xbinherit.js

---
+++ xbinherit.js
@@ -0,0 +1,29 @@
+
+
+var xbClass = function () {
+}
+xbClass.prototype.ctor=function(){
+}
+xbClass.prototype.className="xbClass";
+xbClass.extend = function (props,baseClass) {
+	if(!baseClass){
+		baseClass=xbClass;
+	}
+	if(!props["ctor"]){
+		props["ctor"] = function(){
+			baseClass.prototype["ctor"].apply(this,arguments);
+		}
+	}
+	props["ctor"].prototype=props;
+    for (var k in baseClass.prototype) {
+		if(!props["ctor"].prototype[k])
+			props["ctor"].prototype[k]=baseClass.prototype[k];
+		else if(typeof(baseClass.prototype[k])=="function")
+			props["ctor"].prototype[baseClass.prototype.className+"$"+k] = baseClass.prototype[k];
+    }
+
+	props["ctor"].extend=function(newprops){
+		return xbClass.extend(newprops,props["ctor"]);
+	};
+	return props["ctor"];
+}
